37cc2a54b4a880fc73eb532b08adec68caa9ded4
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-07-01 11:28:20 +0200

    Install various utility gems for testing

--- Gemfile
+++ Gemfile
@@ -11,13 +11,26 @@ gem "coffee-rails", "~> 4.1.0"
 gem "jquery-rails"
 gem "turbolinks", "~> 5.x"
 
+# necessary for guard-rspec
+gem "rspec", "= 3.5.0.beta3"
+gem "rspec-core", "= 3.5.0.beta3"
+gem "rspec-expectations", "= 3.5.0.beta3"
+gem "rspec-mocks", "= 3.5.0.beta3"
+gem "rspec-support", "= 3.5.0.beta3"
+
+gem "rspec-rails", "= 3.5.0.beta3"
+
+gem "rspec-its"
+gem "shoulda-matchers"
+gem "rspec-collection_matchers"
 gem "rails-controller-testing"
-gem "rspec-rails", "3.5.0.beta4"
+gem "factory_girl_rails", "~> 4.7.0"
+gem "faker", "~> 1.6.3"
 
 gem "slim-rails"
 
 gem "devise"
-
+gem "omniauth-github"
 
 group :development, :test do
   gem "byebug", :platform => :mri
@@ -28,4 +41,10 @@ group :development do
   gem "listen", "~> 3.0.5"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem "guard-rspec", "~> 4.7"
+  gem "spring-commands-rspec"
+end
+
+group :test do
+  gem "database_cleaner"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,76 @@
 # frozen_string_literal: true
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
+# Prevent database truncation if the environment is production
 if Rails.env.production?
   abort("The Rails environment is running in production mode!")
 end
+
+require_relative "support/devise_helpers"
+require "devise"
+
 require "spec_helper"
 require "rspec/rails"
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
   config.use_transactional_fixtures = true
 
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+
+  # Database cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  Shoulda::Matchers.configure do |shoulda_config|
+    shoulda_config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, :type => :controller
+  config.include Warden::Test::Helpers
+
+  config.include DeviseHelpers, :type => :controller
+  Warden.test_mode!
+
+  config.after do
+    Warden.test_reset!
+  end
 end
