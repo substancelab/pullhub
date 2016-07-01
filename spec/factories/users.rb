1b70247152d7a34e9e3d55a871cd1683a0dace58
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-07-01 12:53:20 +0200

    Devise user

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
+
+FactoryGirl.define do
+  factory :user, :class => "User" do
+    email
+    password "12345678"
+    password_confirmation "12345678"
+  end
+end
