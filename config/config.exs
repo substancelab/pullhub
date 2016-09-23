4bbf7e655debacc95d7ade3d1116098e5ef2a412
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-09-23 10:02:36 +0200

    Setup uberauth github authentication strategy

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,7 @@ config :pullhub,
 # Configures the endpoint
 config :pullhub, Pullhub.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "qLiu1jabwS9/TyWNzp3M46Q5cFqxDZViNK1b1wYxOA2PDzoUiW6cR/H9B1mDU3aC",
+  secret_key_base: "p2BnMs87klYQkiYcMtlTBzrH1nQX+OHbR0Ixfd61ySN8yJev805GN8MjVVTGV3Te",
   render_errors: [view: Pullhub.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Pullhub.PubSub,
            adapter: Phoenix.PubSub.PG2]
@@ -21,6 +21,15 @@ config :pullhub, Pullhub.Endpoint,
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [ default_scope: "user:email, repo" ] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("GITHUB_CLIENT_ID") || "adec11fb9e64770aeb67",
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET") || "a7eed573d14ecf909e3cde6064e66d8e105fef1a"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
