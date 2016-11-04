0c741fc2c12ae16478e79c70e81f2cc040cee29f
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-11-04 13:41:12 +0100

    Redirect to login page when not signed in + login page design
    This requires us to split the route in two so only the parts that
    require user to be logged in use the authenticate plug.

--- web/plugs/authenticate.ex
+++ web/plugs/authenticate.ex
@@ -1,7 +1,18 @@
 defmodule Pullhub.Plugs.Authenticate do
   import Plug.Conn
 
+
   def init(default), do: default
 
-  def call(conn, default), do: assign(conn, :user, get_session(conn, :current_user))
+  def call(conn, default) do
+    update_session(get_session(conn, :current_user), conn)
+  end
+
+  defp update_session(nil, conn) do
+    Phoenix.Controller.redirect(conn, to: Pullhub.Router.Helpers.auth_path(conn, :index))
+  end
+
+  defp update_session(user, conn) do
+    assign(conn, :user, get_session(conn, :current_user))
+  end
 end
