220d3a643de2d6ba9a5c45dc8cbd2abb62b176a9
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-11-18 12:54:07 +0100

    Setup channel to transmit pull request changes through

---
+++ web/channels/pull_request_channel.ex
@@ -0,0 +1,25 @@
+defmodule Pullhub.PullRequestChannel do
+  use Phoenix.Channel
+
+  def join("pull_request:" <> user_id, _message, socket) do
+    {user_id, _} = Integer.parse(user_id)
+
+    %{id: id} = socket.assigns[:user]
+
+    case id == user_id do
+    true ->
+      {:ok, socket}
+    false ->
+      {:error, "This is not your solo channel!"}
+    end
+  end
+
+  def handle_in("update_pull_requests", %{}, socket) do
+    %{id: user_id} = socket.assigns[:user]
+
+    %{user: Pullhub.Repo.get!(Pullhub.User, user_id)}
+    |> Pullhub.PullRequestsFetcher.fetch
+
+    {:noreply, socket}
+  end
+end
