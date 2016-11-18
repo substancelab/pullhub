c24f08b6a4b8b627112d5ec5f1b018ae6b7d5c81
Jakob Dam Jensen <jakob@nopointlessapps.com>
2016-11-18 12:55:11 +0100

    Setup JS pull request websocket connection
    List can be reloaded by hittin the "r" key

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,9 +3,11 @@ import 'phoenix_html'
 import socketConnector from './socket'
 
 import startRepositoryChannel from './channels/repository_channel'
+import startPullRequestChannel from './channels/pull_request_channel'
 
 let userId = $("meta[name='user_id']").attr('content')
 let userToken = $("meta[name='user_token']").attr('content')
 
 let socket = socketConnector.connect(userToken)
 startRepositoryChannel(socket, userId)
+startPullRequestChannel(socket, userId)
---
+++ web/static/js/channels/pull_request_channel.js
@@ -0,0 +1,36 @@
+/* global $ */
+export default function pullRequestChannelHandler (socket, userId) {
+  if ($('#pullrequest-fetch-info').length === 0) {
+    return
+  }
+
+  let channel = socket.channel(`pull_request:${userId}`, {})
+  channel.join()
+    .receive('ok', resp => {
+      updateList()
+    })
+    .receive('error', resp => { console.log('Unable to join', resp) })
+
+  function updateList () {
+    $('#pullrequest-fetch-info').show()
+    channel.push('update_pull_requests', {})
+  }
+
+  channel.on('pull_requests_updated', payload => {
+    payload.data.forEach((info) => {
+      let content = info.rendered_pull_requests
+      $(`#repository-pull-requests-${info.repository_id} tbody`).html(content)
+    })
+    $('#pullrequest-fetch-info').hide()
+
+    setTimeout(updateList, 120000)
+  })
+
+  window.addEventListener('keyup', function (e) {
+    if (e.key === 'r') {
+      updateList()
+    }
+  })
+
+  return channel
+}
